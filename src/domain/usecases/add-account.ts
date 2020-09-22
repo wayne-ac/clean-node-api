f495e5ea3b66c47ead5caf29644710b8a63f05c1
wayne-ac <wayne.conceicao@gmail.com>
2020-09-21 21:13:19 -0300

    refactor: transform handle to async

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -7,5 +7,5 @@ export interface AddAccountModel {
 }
 
 export interface AddAccount {
-  add: (accountModel: AddAccountModel) => AccountModel
+  add: (accountModel: AddAccountModel) => Promise<AccountModel>
 }
