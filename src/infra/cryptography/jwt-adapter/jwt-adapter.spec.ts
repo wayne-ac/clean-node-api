cf4dfbdf6e0a9debac19c6c083b4f7bce3594246
wayne-ac <wayne.conceicao@gmail.com>
2021-03-24 20:34:15 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -7,7 +7,7 @@ jest.mock('jsonwebtoken', () => ({
   },
 
   async verify (): Promise<string> {
-    return 'any_token'
+    return 'any_value'
   }
 }))
 
@@ -44,6 +44,12 @@ describe('Jwt Adapter', () => {
       const verifySpy = jest.spyOn(jwt, 'verify')
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
+    })
+
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
     })
   })
 })
