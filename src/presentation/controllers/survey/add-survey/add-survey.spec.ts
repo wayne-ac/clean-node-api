7eee43e1db1ba2351372b28c147239a8f3996e38
wayne-ac <wayne.conceicao@gmail.com>
2021-04-14 20:35:50 -0300

    refactor: change AddSurvey to insert a date also

--- src/presentation/controllers/survey/add-survey/add-survey.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey.spec.ts
@@ -1,6 +1,7 @@
 import { AddSurveyController } from './add-survey-controller'
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper'
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -44,6 +45,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AddSurvey Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('Should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
