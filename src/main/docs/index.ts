24a2a7d81f98aff1fd5293f72e9b9a2f2eee4cff
wayne-ac <wayne.conceicao@gmail.com>
2021-04-24 18:32:40 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,24 @@
+import { loginPath } from './paths'
+import { accountSchema, loginParamsSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
