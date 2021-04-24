1abaace363ef311f66a6bd7af16274f2aa5dca23
wayne-ac <wayne.conceicao@gmail.com>
2021-04-24 15:45:44 -0300

    feat: add swagger api

--- src/main/factories/usecases/account/authentication/db-authentication-factory.ts
+++ src/main/factories/usecases/account/authentication/db-authentication-factory.ts
@@ -3,7 +3,7 @@ import { DbAuthentication } from '@/data/usecases/account/authentication/db-auth
 import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '@/infra/cryptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import { Authentication } from '@/domain/usecases/authentication'
+import { Authentication } from '@/domain/usecases/account/authentication'
 
 export const makeDbAuthentication = (): Authentication => {
   const salt = 12
